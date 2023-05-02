7e24e1688d50d3393566e21d2b8069ab0042eb16
stack <songshoubing7664@163.com>
2023-05-03 00:26:51 +0800

    Site updated: 2023-05-03 00:26:50

---
+++ website/static/js/promotionPlatform.js
@@ -0,0 +1,15 @@
+function displayPromotionPlatformCodes() {
+    let codeHTML = `
+      <div>
+        <span><a class="red" href="https://freelibrary.top/posts/32631bce.html" target="_blank">自媒体涨粉平台</a></span> |
+        <span><a href="https://freelibrary.top/posts/481424ab.html" target="_blank">信用卡推广平台</a></span> |
+        <span><a class="red" href="https://freelibrary.top/posts/32631bce.html" target="_blank">全网项目资源整合平台</a></span> |
+        <span><a href="https://freelibrary.top/posts/c0554505.html" target="_blank">百度网盘「官方」超低价SVIP会员</a></span>
+      </div>
+    `;
+    return codeHTML;
+}
+  
+export default {
+    displayPromotionPlatformCodes
+};
