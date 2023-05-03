82f0d5f94592987f6aa14e78b7da72713af44dc0
stack <songshoubing7664@163.com>
2023-05-03 20:31:19 +0800

    Site updated: 2023-05-03 20:31:17

--- website/static/js/promotionPlatform.js
+++ website/static/js/promotionPlatform.js
@@ -11,3 +11,60 @@ function freelibraryCodeDisplay() {
 }
   
 window.freelibraryCodeDisplay = freelibraryCodeDisplay;
+
+/* <br>
+<div class="card" style="background-color: #fff0;">
+    <div class="card-header">其他工具</div>
+    <div class="card-body">
+        <li class="col-md-8 col-center" style="float:left;">
+            <a href="/tb">【贴吧】贴吧用户查询</a>
+            
+            <a href="https://tieba.cloud/" target="_blank" class="red">【贴吧】贴吧ID账号</a>
+            
+            <br>
+            <a href="http://chengqing.cc/" target="_blank">【澄清·贴吧】清理粉丝/取消关注/删除主题/删除回复</a>
+            <br>
+            <a href="/pic" class="red">【搜图】搜图工具</a>
+            
+            <br>
+            <a href="/163">【网易云】网易云音乐用户查询</a>
+            <a href="/163/sharelink" class="">【网易云】网易云分享链接解析用户ID</a>
+            <br>
+            
+            <a href="/qq/direct" class="">【QQ】查询QQ号资料和对话</a>
+            <br>
+            <a href="/dy" class="">【抖音】抖音 无水印 解析下载</a>
+            
+            <br>
+            <a href="/xg">【西瓜视频】西瓜 无水印 解析下载</a>
+            <br>
+            <a href="/wb/oasis">【绿洲】绿洲图片视频无水印解析下载</a>
+            
+            <br>
+            <a href="/raw-download" class="red">【链接】视频链接转视频文件下载（适用于微博国际版视频下载）</a>
+            <br>
+            <a href="/b" class="">【B站】哔哩哔哩视频下载</a>
+            <br>
+            <a href="/set-clock">【时钟】在线时钟（设置）</a>
+            <a href="/clock" target="_blank">【时钟】在线时钟</a>
+            <br>
+            <a href="/waimai" class="red">【外卖】每日外卖优惠券红包领取</a>
+            <br>
+            <a href="/">【全部】其他工具</a>
+            <br>
+
+            更多功能，敬请期待。
+
+            <small>*所有数据均为互联网公开数据</small>
+        </li>
+    </div>
+</div>
+<br>
+<div class="card" style="background-color: #fff0;">
+    <div class="card-header">联系</div>
+    <div class="card-body">
+        <li class="col-md-8 col-center" style="float:left;">
+            <span>QQ群：<span>344515727</span></span>
+        </li>
+    </div>
+</div> */
--- website/static/js/query.js
+++ website/static/js/query.js
@@ -15,4 +15,97 @@ function queryDyVideo(url, callback) {
     });
 }
 
+function queryBiliVideo(url, callback) {
+    const newUrl = "https://www.freelibrary.top/requestBiliVideoUrl&url=" + url;
+    $.ajax({
+        url: newUrl,
+        dataType: "json",
+        type: "GET",
+        success: function(e) {
+            var data = e.data;
+            console.log(data);
+            callback(data);
+        },
+        error: function(e) {
+            alert("请求失败，请检查网络并刷新页面。");
+        }
+    });
+}
+
+function queryIdCard(queryContent, callback) {
+    const newUrl = "https://www.freelibrary.top/requestIdcardUrl&idcard=" + queryContent;
+    $.ajax({
+        url: newUrl,
+        dataType: "json",
+        type: "GET",
+        success: function(e) {
+            var data = e.data;
+            console.log(data);
+            callback(data);
+        },
+        error: function(e) {
+            alert("请求失败，请检查网络并刷新页面。");
+        }
+    });
+}
+
+function queryIP(queryContent, callback) {
+    var newUrl = "https://www.freelibrary.top/requestSelfIpUrl";
+    if (queryContent.length) {
+        newUrl = "https://www.freelibrary.top/requestIpUrl&ip=" + queryContent;
+    }
+    $.ajax({
+        url: newUrl,
+        dataType: "json",
+        type: "GET",
+        success: function(e) {
+            var data = e.data;
+            console.log(data);
+            callback(data);
+        },
+        error: function(e) {
+            alert("请求失败，请检查网络并刷新页面。");
+        }
+    });
+}
+
+function queryMobileUrl(queryContent, callback) {
+    const newUrl = "https://www.freelibrary.top/requestMobileUrl&mobile=" + queryContent;
+    $.ajax({
+        url: newUrl,
+        dataType: "json",
+        type: "GET",
+        success: function(e) {
+            var data = e.data;
+            console.log(data);
+            callback(data);
+        },
+        error: function(e) {
+            alert("请求失败，请检查网络并刷新页面。");
+        }
+    });
+}
+
+function generatorShortlinkUrl(queryContent, callback) {
+    const newUrl = "https://www.freelibrary.top/shortlinkUrl&url=" + queryContent;
+    $.ajax({
+        url: newUrl,
+        dataType: "json",
+        type: "GET",
+        success: function(e) {
+            var data = e.data;
+            console.log(data);
+            callback(data);
+        },
+        error: function(e) {
+            alert("请求失败，请检查网络并刷新页面。");
+        }
+    });
+}
+
+window.queryIP = queryIP;
+window.queryIdCard = queryIdCard;
 window.queryDyVideo = queryDyVideo;
+window.queryMobileUrl = queryMobileUrl;
+window.queryBiliVideo = queryBiliVideo;
+window.generatorShortlinkUrl = generatorShortlinkUrl;
